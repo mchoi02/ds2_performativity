81ef4a6f4da66bb5d089d7d138062e1b8d56d1cb
Minjun <mchoi02@risd.edu>
2018-03-15 11:13:22 -0400

    Test

---
+++ script.js
@@ -0,0 +1,42 @@
+window.onload = function(){
+// Get the modal
+var modal =
+document.getElementById("modaltz1");
+document.getElementById("modalsteps1");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img =
+document.getElementById("tz1");
+document.getElementById("steps1");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+}
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
